66441c46edef92fdd9cc387879c67564f7ec504b
Silviu Leonard Vatamanelu <silviu48vatamanelu@gmail.com>
2019-05-31 12:15:40 +0200

    JS Start

--- www/js/index.js
+++ www/js/index.js
@@ -1,16 +1,43 @@
-var italianImage = "";
-var englishImage = "";
-var currentImage = "italian0.jpg";
+var root = "img/";
+var italianImage = "italian";
+var englishImage = "english";
+var currentImage = "italian";
+var extension = ".jpg";
 
 var pageNum = 0;
 
 function toItalian(){
     
-    
-    
+    currentImage = italianImage;
+    document.getElementById("image").src = root + currentImage + pageNum + extension;
+    console.log(currentImage + pageNum + extension);
 }
 
 function toEnglish(){
     
+    alert("In Sviluppo");
+}
+
+function changeImage() {
+
+    switch (pageNum) {
+        case 0:
+            pageNum++;
+            document.getElementById("image").src = root + currentImage + pageNum + extension;
+            break;
+
+        case 1:
+            pageNum++;
+            document.getElementById("image").src = root + currentImage + pageNum + extension;
+            document.getElementById("confirm").innerHTML = "Inizio";
+            document.getElementById("confirm").onclick = "nextPage()";
+            break;
+    }
+
+}
+
+function nextPage(){
+    
+    window.open("caricamento.html");
     
 }
