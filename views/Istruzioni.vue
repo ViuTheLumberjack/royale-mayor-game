f3a95865d32fdeae985329e8f7a284a36cae4175
Silviu Leonard Vatamanelu <silviu48vatamanelu@gmail.com>
2019-06-11 09:58:47 +0200

    Changes

---
+++ views/Istruzioni.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="myImage">
+           <div id="language">
+              <button id="italian" @click="toItalian"><img src="../../assets/IT.png" class="ButtonIMG"></button>
+              <button id="english" @click="toEnglish"><img src="../../assets/ENG.jpg" class="ButtonIMG"></button>
+           </div>
+           <img src="../../assets/italian0.jpg" alt="pagina" id="image" class="sfondo">
+           <button id="confirm" v-on:click.prevent="changeImage"><img src="../../assets/avanti.png" class="avantiIMG"></button>
+    </div>
+</template>
+
+<script >
+export default {
+  name: 'HelloWorld'
+}
+</script>
