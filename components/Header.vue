6b89497ecd9486ae93d8821b0e1688e0f0d8cee9
Silviu Leonard Vatamanelu <silviu48vatamanelu@gmail.com>
2019-06-26 11:04:05 +0200

    Major Fixes

--- components/Header.vue
+++ components/Header.vue
@@ -1,9 +1,9 @@
 <template>
     <div id = "head">
-        <img src='../assets/bordo.png' class="bordo">
+        <img src='../assets/header/bordo.png' class="bordo">
         <nav>
-            <button id="italian" v-on:click.prevent="toItalian" :disabled="italianButton"><img src="../assets/IT.png" class="ButtonIMG"></button>
-            <button id="english" v-on:click.prevent="toEnglish" :disabled="englishButton"><img src="../assets/ENG.jpg" class="ButtonIMG"></button>     
+            <button id="italian" v-on:click.prevent="toItalian" :disabled="italianButton"><img src="../assets/header/IT.png" class="ButtonIMG"></button>
+            <button id="english" v-on:click.prevent="toEnglish" :disabled="englishButton"><img src="../assets/header/ENG.jpg" class="ButtonIMG"></button>     
         </nav>
     </div>
 </template>
@@ -41,4 +41,4 @@
 
 </script>
 
-<style src="../css/Header.css"></style>
+<style src="../css/header.css"></style>
--- components/Istruzioni.vue
+++ components/Istruzioni.vue
@@ -1,27 +1,27 @@
 <template>
     <div id="myImage">
       <img v-bind:src = "img_source" alt = "pagina" id="image" class="sfondo">
-      <span v-html="button"></span>
+      <button id = "confirm" v-on:click.prevent="changeImage"><img v-bind:src="button_image" class="avantiIMG" alt="bottone"></button>
+      <!-- <span v-html="button"/> -->
     </div>
 </template>
 
 <script>
-  import { image_info } from '../js/constants'
+  // import { image_info } from '../js/constants'
   import Header from './Header'
+  import { it_images } from "../assets/istruzioni/index.js"
 
-  var button_image = require("../assets/avanti.png");
-  var image_class = "avantiIMG";
-  var button = '<button id = "confirm" v-on:click.prevent="changeImage"><img v-bind:src="' + button_image +'" v-bind:class="' + image_class + '" alt="bottone"></button>';
+  var button_image = it_images.avanti;
+  var button = '<button id = "confirm" v-on:click.prevent="changeImage"><img v-bind:src="' + button_image +'" class="avantiIMG" alt="bottone"></button>';
   var pageNum = 0;
   var currentImage = Header.data().currentLanguage;
-  var img_source = require('../assets/italian0.jpg');
+  var img_source = it_images.italian0;
 
   export default {
     name: 'Istruzioni',
     data() {
       return{
-        button_image,
-        image_class,
+        button_image: it_images.avanti,
         button,
         pageNum,
         currentImage,
@@ -29,30 +29,33 @@
       }
     },
     
-    computed:{
+    methods:{
       changeImage() {
 
               switch (pageNum) {
                   case 0: 
                       pageNum++;
-                      img_source = image_info.currentImage + pageNum + image_info.extension;
+                      img_source = it_images.italian1;
                       break;
 
                   case 1:
                       
                       pageNum++;
-                      img_source = image_info.root + image_info.currentImage + pageNum + image_info.extension;
-                      if (currentImage === image_info.ITALIAN_IMAGE)
-                      button_image = require("../assets/inizia.png");
-                      else button_image = require("../assets/start.png");
+                      img_source = it_images.italian2;
+                      /*if (currentImage === image_info.ITALIAN_IMAGE)
+                      button_image = ;
+                      else button_image = ;*/
 
-                      image_class = "iniziaIMG";
+                       // image_class = "iniziaIMG";
                       break;
           
                   case 2:
 
                       button = '<router-link to="/selezione"/>';
                       break;
+                  
+                  default : 
+                    console.log(pageNum);
                   
               }
           
