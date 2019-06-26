57efd2a1b83e41aa31524ef3d28a59f3c08e54c5
Silviu Leonard Vatamanelu <silviu48vatamanelu@gmail.com>
2019-06-26 11:24:08 +0200

    sm

--- components/Istruzioni.vue
+++ components/Istruzioni.vue
@@ -1,7 +1,7 @@
 <template>
     <div id="myImage">
-      <img v-bind:src = "img_source" alt = "pagina" id="image" class="sfondo">
-      <button id = "confirm" v-on:click.prevent="changeImage"><img v-bind:src="button_image" class="avantiIMG" alt="bottone"></button>
+      <img v-bind:src = "getImage()" alt = "pagina" id="image" class="sfondo">
+      <button id="confirm" v-on:click.prevent = "changeImage"><img v-bind:src = "button_image" class="avantiIMG" alt="bottone"></button>
       <!-- <span v-html="button"/> -->
     </div>
 </template>
@@ -15,33 +15,34 @@
   var button = '<button id = "confirm" v-on:click.prevent="changeImage"><img v-bind:src="' + button_image +'" class="avantiIMG" alt="bottone"></button>';
   var pageNum = 0;
   var currentImage = Header.data().currentLanguage;
-  var img_source = it_images.italian0;
+  // var img_source = it_images.italian0;
 
   export default {
     name: 'Istruzioni',
     data() {
       return{
-        button_image: it_images.avanti,
+        button_image,
         button,
         pageNum,
         currentImage,
-        img_source,
+        // img_source,
       }
     },
     
     methods:{
       changeImage() {
-
+        console.log(pageNum);
               switch (pageNum) {
                   case 0: 
                       pageNum++;
-                      img_source = it_images.italian1;
+                      // img_source = it_images.italian1;
+                      
                       break;
 
                   case 1:
                       
                       pageNum++;
-                      img_source = it_images.italian2;
+                      // img_source = it_images.italian2;
                       /*if (currentImage === image_info.ITALIAN_IMAGE)
                       button_image = ;
                       else button_image = ;*/
@@ -55,11 +56,39 @@
                       break;
                   
                   default : 
-                    console.log(pageNum);
+                    
                   
               }
           
-          }
+          },
+      getImage(){
+        var img_source;
+
+        switch (pageNum) {
+                  case 0: 
+                      
+                      img_source = it_images.italian0;
+                      
+                      break;
+
+                  case 1:
+                      
+                      
+                      img_source = it_images.italian1;
+                      /*if (currentImage === image_info.ITALIAN_IMAGE)
+                      button_image = ;
+                      else button_image = ;*/
+
+                       // image_class = "iniziaIMG";
+                      break;
+          
+                  case 2:
+                    img_source = it_images.italian2;
+                  
+        }
+
+        return img_source;
+      }
     }
   }
 </script>
