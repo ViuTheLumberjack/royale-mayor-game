02b5bece1b21945139eef07c9a4de5c42fb540a9
Silviu Leonard Vatamanelu <silviu48vatamanelu@gmail.com>
2019-09-30 20:02:33 +0200

    CSI

--- components/Fine.vue
+++ components/Fine.vue
@@ -9,6 +9,12 @@
             <td> {{ i }} </td>
             <td> {{ $route.params.events[ i - 1] }} </td>
             <td v-for = "(index, t) in $route.params.teams" :key = "index"> {{ $route.params.risultati[t][i-1] }} </td>
+            <!--   -->
+        </tr>
+        <tr>
+            <td> CSI </td>
+            <td>  </td>
+            <td v-for = "(index, t) in $route.params.teams" :key = "index"> {{ csi(t) }} </td>
         </tr>
     </div>
 </template>
@@ -18,6 +24,33 @@ export default {
     name: "Fine",
     data(){
         return{
+
+        }
+    },
+    methods: {
+        csi(t){
+            var yy = 0
+            var yn = 0
+            var ny = 0
+            //var nn = 0
+            var res = 0
+            for(var i = 0; i < 10; i++){
+                if(this.$route.params.risultati[t][i-1] === true && this.$route.params.events[i] === true){
+                    yy++
+                } else if(this.$route.params.risultati[t][i-1] === true && this.$route.params.events[i] === false){
+                    yn++
+                } else if(this.$route.params.risultati[t][i-1] === false && this.$route.params.events[i] === true){
+                    ny++
+                } else {
+                    //nn++
+                }
+            }
+
+            res = yy/(yy+yn+ny);
+
+            res = res.toFixed(2);
+
+            return res;
 
         }
     }
