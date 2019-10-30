3e274d5f1b3e22a4b2c5dbd1b9304b0c9389c5cc
Leonardo <leonardo.fontana.spamm@gmail.com>
2019-10-30 19:45:24 +0100

    First Page style, Image change,, Enlgish image added

--- components/Language.vue
+++ components/Language.vue
@@ -1,7 +1,9 @@
 <template>
     <div id=lingua>
-    	<router-link :to="{name:'Istruzioni', params: {lang : lang} }"><img src="../assets/header/IT.png" class="ButtonIMG" @click="changeLanguage('it')"></router-link>
-        <router-link :to="{name:'Istruzioni', params: {lang : lang} }"><img src="../assets/header/ENG.jpg" class="ButtonIMG" @click="changeLanguage('eng')"></router-link>
+        <h1 id="Testo"> SELEZIONA LA LINGUA </h1>
+        <h1 id="Testo"> CHOOSE YOUR LANGUAGE </h1>
+    	<router-link :to="{name:'Istruzioni', params: {lang : lang} }"><img id= "IT" src="../assets/header/IT.png" class="ButtonIMG" @click="changeLanguage('it')"></router-link>
+        <router-link :to="{name:'Istruzioni', params: {lang : lang} }"><img id="ENG" src="../assets/header/ENG.jpg" class="ButtonIMG" @click="changeLanguage('eng')"></router-link>
     </div>
 </template>
 
@@ -26,6 +28,4 @@ export default {
 }
 </script>
 
-<style>
-
-</style>
+<style  src="../css/Language.css"></style>
